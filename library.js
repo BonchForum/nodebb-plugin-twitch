4ccab21af16665697c864ec4b4794b3c3d68dd84
luchanso <oleg@loutchansky.com>
2016-01-30 15:07:46 +0300

    Fix regex

--- library.js
+++ library.js
@@ -3,7 +3,7 @@
 		var Twitch = {},
 			embed = '<iframe src="http://player.twitch.tv/?channel=$1" frameborder="0" scrolling="no" height="378" width="620"></iframe>';
 
-		var regularUrl = /(https:\/\/www\.twitch\.tv\/)(\w+)/g;
+		var regularUrl = /<a href="https:\/\/www\.twitch\.tv\/(\w+)"<\/a>/g;
 
 		Twitch.parse = function(data, callback) {
 			if (!data || !data.postData || !data.postData.content) {
@@ -11,8 +11,6 @@
 			}
 
 			if (data.postData.content.match(regularUrl)) {
-				console.log('HHEWSASSS');
-
 				data.postData.content = data.postData.content.replace(regularUrl, embed);
 		}
 
