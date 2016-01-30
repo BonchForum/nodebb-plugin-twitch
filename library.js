b8f95b7b34fe1290a9386ec9236a381f79cad994
luchanso <oleg@loutchansky.com>
2016-01-30 14:56:30 +0300

    Fix

--- library.js
+++ library.js
@@ -1,20 +1,20 @@
 (function(module) {
-	"use strict";
-	var Twitch = {},
-		embed = '<iframe src="http://player.twitch.tv/?channel=$1" frameborder="0" scrolling="no" height="378" width="620"></iframe>';
+		"use strict";
+		var Twitch = {},
+			embed = '<iframe src="http://player.twitch.tv/?channel=$1" frameborder="0" scrolling="no" height="378" width="620"></iframe>';
 
-	var	regularUrl = /(https:\/\/www\.twitch\.tv\/)(\w+)/g;
+		var regularUrl = /(https:\/\/www\.twitch\.tv\/)(\w+)/g;
 
-	Twitch.parse = function(data, callback) {
-		if (!data || !data.postData || !data.postData.content) {
-			return callback(null, data);
+		Twitch.parse = function(data, callback) {
+			if (!data || !data.postData || !data.postData.content) {
+				return callback(null, data);
+			}
+
+			if (data.postData.content.match(regularUrl)) {
+				console.log('HHEWSASSS');
+
+				data.postData.content = data.postData.content.replace(regularUrl, embed);
 		}
-
-		if (data.postData.content.match(regularUrl)) {
-      console.log('HHEWSASSS');
-			
-      data.postData.content = data.postData.content.replace(regularUrl, embed));
-		}		
 
 		callback(null, data);
 	};
